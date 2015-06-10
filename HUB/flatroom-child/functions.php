a9da8fcf062ada104e8ad40f0d8327fcf80f1e91
Chris Hall <chris@weareatom.com>
2015-06-10 02:54:56 -0300

    Plugins added to HUB. SVG support added to social icons.

--- HUB/flatroom-child/functions.php
+++ HUB/flatroom-child/functions.php
@@ -936,4 +936,14 @@ class Team_Widget extends WP_Widget
 }
 
 
+
+//Allow SVG support
+
+function cc_mime_types($mimes){
+	$mimes['svg'] = 'image/svg+xml';
+	return $mimes;
+}
+add_filter('upload_mimes', 'cc_mime_types');
+
+
 ?>
